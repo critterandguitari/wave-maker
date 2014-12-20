d5223d09653c22dfb419ef32ef5316158352fe05
Owen <owen@Owens-MacBook-Pro.local>
2014-12-20 03:31:18 -0500

    drawing waves

--- one-sine.py
+++ one-sine.py
@@ -4,6 +4,6 @@ import math
 for i in range(-1,258):
     sin1 = math.sin((i / 256.0) * 2. * math.pi)
     val = sin1
-    print "{:.10f}".format(val) + "f,"
+    print "{:.10f}".format(val) + "f,",
     
     
